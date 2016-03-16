6dfa73056e1df48302edf1e2d84396a2059a8bac
Pavel Gladkov <webgeist.xor@gmail.com>
2016-03-16 21:01:35 +0300

    init

--- drobot.py
+++ drobot.py
@@ -1,6 +1,7 @@
 import logging
 from telegram import Updater
 from config import TOKEN
+from random import randint
 
 logging.basicConfig(
         format='%(asctime)s - %(name)s - %(levelname)s - %(message)s',
@@ -9,18 +10,29 @@ logging.basicConfig(
 logger = logging.getLogger(__name__)
 
 
+DELAY_DICT = {}
+
+
 def start(bot, update):
-    bot.sendMessage(update.message.chat_id, text='Привет! Я drobot. Всем жму руку.')
+    bot.sendMessage(update.message.chat_id, text='Привет! Я drobot. Жму руку.')
 
 
 def help(bot, update):
-    bot.sendMessage(update.message.chat_id, text='Я drobot. Всем жму руку.')
+    bot.sendMessage(update.message.chat_id, text='Я drobot. Жму руку.')
 
 
 def message(bot, update):
-    author = update.message.from_user.first_name
-    text = ', '.join([author, 'жму руку!'])
-    bot.sendMessage(update.message.chat_id, text=text)
+
+    if update.message.chat_id not in DELAY_DICT:
+        DELAY_DICT[update.message.chat_id] = randint(0, 10)
+    else:
+        DELAY_DICT[update.message.chat_id] -= 1
+
+    if DELAY_DICT[update.message.chat_id] == 0:
+        DELAY_DICT.pop(update.message.chat_id)
+        author = update.message.from_user.first_name
+        text = ', '.join([author, 'жму руку!'])
+        bot.sendMessage(update.message.chat_id, text=text)
 
 
 def error(bot, update, error):
